a6bce13e3ce7ec09de7b79baaa10dc171415e622
Strikingwolf <Strikingwolf2012@gmail.com>
2014-08-11 17:01:58 -0500

    Added some client rendering/sounds
    stuff :P

---
+++ src/main/java/mods/vmcm/proxy/ClientProxy.java
@@ -0,0 +1,18 @@
+package mods.vmcm.proxy;
+
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
+public class ClientProxy extends CommonProxy
+{
+    @Override
+    public void initSounds()
+    {
+
+    }
+
+    @Override
+    public void initRenderers()
+    {
+
+    }
+}
